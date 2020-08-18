cc794fcd35a9f6c6838f60d6a189c36c4e26f1f6
longTimeNoSee <johnberboy@naver.com>
2020-08-19 00:16:48 +0900

    Model, admin, veiw, form, generic view

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# SQL에 데이터를 저장할 때 column을 정의한다.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # CASCADE는 참조하는 PK가 삭제되면, 해당 FK도 같이 삭제하겠다는 의미.
+    # 1대다 관계
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,19 @@ from . import views
 
 # 최상위 urls에서 url을 파싱하고 받아가지고, 
 # path별로 다른 앱으로 분기시켜준다. 
+app_name = 'polls'
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     # ex: /polls/5/
+#     path('<int:question_id>/', views.detail, name='detail'),
+#     # ex: /polls/5/results/
+#     path('<int:question_id>/results/', views.results, name='results'),
+#     # ex: /polls/5/vote/
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
